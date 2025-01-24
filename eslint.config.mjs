c5ebe071d64b80cd923f81ab014093074597ae25
kbpolra <kbpolra@myseneca.ca>
2025-01-24 18:33:00 -0500

    adding jest testing and unit testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,17 @@
+// eslint.config.mjs
+
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: globals.node } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
