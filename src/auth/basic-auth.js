30c6d6603dedcbeb5bb38e0b90df8278cd6aa1a0
kbpolra <kbpolra@myseneca.ca>
2025-02-07 21:21:22 -0500

    Work with a hashed email vs. plain text version for data privacy

--- src/auth/basic-auth.js
+++ src/auth/basic-auth.js
@@ -4,8 +4,9 @@
 // https://github.com/http-auth/http-auth-passport
 
 const auth = require('http-auth');
-const passport = require('passport');
 const authPassport = require('http-auth-passport');
+// We'll use our authorize middle module
+const authorize = require('./auth-middleware');
 
 const logger = require('../logger');
 
@@ -26,4 +27,5 @@ module.exports.strategy = () =>
     })
   );
 
-module.exports.authenticate = () => passport.authenticate('http', { session: false });
+// Now we'll delegate the authorization to our authorize middleware
+module.exports.authenticate = () => authorize('http');
